c223c472653b757445b30d6b46d805de7388190a
houjia <931953294@qq.com>
2022-05-07 15:27:43 +0800

    .

---
+++ sign.go
@@ -0,0 +1,21 @@
+package verifyEthSig
+
+import (
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func SignHash(hash []byte, priKey string) (sigStr string, err error) {
+	privateKey, err := crypto.HexToECDSA(priKey)
+	if err != nil {
+		return
+	}
+	hash = signByteHash(hash)
+
+	signature, err := crypto.Sign(hash, privateKey)
+	if err != nil {
+		return
+	}
+	signature[64] += 27
+	return hexutil.Encode(signature), nil
+}
