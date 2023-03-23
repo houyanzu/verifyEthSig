30c4cb6ffd219b83f2b5f48f56eb53c72ecbe015
admin <support@omnispheredao.com>
2023-03-23 15:05:54 +0800

    .

--- sign.go
+++ sign.go
@@ -19,3 +19,18 @@ func SignHash(hash []byte, priKey string) (sigStr string, err error) {
 	signature[64] += 27
 	return hexutil.Encode(signature), nil
 }
+
+func SignStr(data, priKey string) (sigStr string, err error) {
+	privateKey, err := crypto.HexToECDSA(priKey)
+	if err != nil {
+		return
+	}
+	hash := signStrHash(data)
+
+	signature, err := crypto.Sign(hash, privateKey)
+	if err != nil {
+		return
+	}
+	signature[64] += 27
+	return hexutil.Encode(signature), nil
+}
